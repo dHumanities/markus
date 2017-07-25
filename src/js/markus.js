45516e0b9ae7505f2593e548e16fa3165216a646
Mees Gelein <meesgelein@gmail.com>
2017-07-25 10:38:20 +0200

    Added documentation to markus.js

--- src/js/markus.js
+++ src/js/markus.js
@@ -1,8 +1,19 @@
+/**
+ * Configuration object. Holds the version and build number
+ * @type {Object}
+ */
 var markus = {
     version: "0.3.4.1",
     build: "201602005"
 };
 
+/**
+ * New JQuery function definition. Scrolls to the element that is passed
+ * as a parameter: e.g:
+ * $('#div_element2').goTo();
+ *
+ * Source: https://stackoverflow.com/questions/4801655/how-to-go-to-a-specific-element-on-page
+ */
 ( function($) {
 $.fn.goto = function() {
     $('html, body').animate({
@@ -12,6 +23,13 @@ $.fn.goto = function() {
 }
 } )(window.jQuery);
 
+/**
+ * New JQuery function definition. Replaces all elements passed to this functions
+ * with the specified element name. For example, to replace all divs with spans:
+ * $('div').replaceTagName('span');
+ *
+ * Source: https://stackoverflow.com/questions/2815683/jquery-javascript-replace-tag-type
+ */
 ( function($) {
 $.fn.replaceTagName = function(replaceWith) {
     var tags = [],
