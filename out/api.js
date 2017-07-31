8d6425fb0b2b6c6fecb0e482c37291f1d3cd8c5e
Mees Gelein <meesgelein@gmail.com>
2017-07-31 14:39:10 +0200

    Regenerated YUIDoc docs

---
+++ out/api.js
@@ -0,0 +1,27 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Global",
+        "JQuery",
+        "WebDictionary",
+        "webDictionary.js_anonymous"
+    ],
+    "modules": [
+        "markus.js",
+        "webDictionary.js"
+    ],
+    "allModules": [
+        {
+            "displayName": "markus.js",
+            "name": "markus.js",
+            "description": "Markus.js base file"
+        },
+        {
+            "displayName": "webDictionary.js",
+            "name": "webDictionary.js",
+            "description": "webDictionary.js file. This file contains the webDictionary definitions"
+        }
+    ],
+    "elements": []
+} };
+});
