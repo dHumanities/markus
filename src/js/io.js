9e95d9356637db0d5cf9e0b9e2aa8e537092e27e
Mees Gelein <meesgelein@gmail.com>
2017-08-01 13:51:33 +0200

    Started Docs for io.js

--- src/js/io.js
+++ src/js/io.js
@@ -1,53 +1,104 @@
+/**
+ * io.js base file.
+ * @module io.js
+ */
+
+ /**
+ The class that holds the variables and functions created in the namespace of this file.
+ The constrcutor of this object is only called once, because it is an anonymous function.
+
+ The functions defined in this class are later added to the MARKUS global object and can
+ from that point on be called as `markus.io.FUNCTION_NAME`
+
+ @class io.js_anonymous
+ @constructor
+ @param {Object}  _m a reference to the Markus Configuration Object is passed
+ **/
 ( function(_m) {
+/**
+ * Creates an error Handler function. This function prints out the error message
+ * prefaced by 'Error', to make it easier to spot in the console log.
+ *
+ * @method ERROR_HANDLER
+ * @param  {Error} e The error that has happened
+ */
 var ERROR_HANDLER = function(e) {
     console.log('Error: ' + e.message);
 };
 
+/**
+ * Updates the color switchers for the tagNames. This is called on load. It
+ * hides or shows buttons depending on their visibility and changes the color-switcher-class
+ * based on their status
+ *
+ * @method updateSwitchers
+ */
 var updateSwitchers = function() {
+    //If there are buttons on the buttonsRow
     if ($("#buttonsRow").length > 0) {
+
+        //For each of the tagNames that have been defined in the markus tag module
         for (var tagName in _m.tag) {
+            //Either use the predefined buttonName or just the tagname and get the status of the tag
             var buttonName = _m.tag[tagName]["buttonName"] || tagName;
             var status = _m.tag[tagName]["status"] || "";
+
+            //If we can't find the color-switcher-class for that tagName
             if ($("#buttonsRow").find("button[color-switcher-class='" + tagName + "']").length == 0) {
+                //We add a color-switcher-class for that tagName
                 $("#buttonsRow").prepend('<button type="button" class="btn btn-sm switcher ' + tagName + ' ' + status + '" color-switcher-class="' + tagName + '" data-toggle="tooltip" data-placement="auto" title="" data-original-title="click to change color">' + buttonName + '</button> ');
             } else {
                 if (_m.tag[tagName]["buttonName"]) {
+                  //Else just update the class and add 'status'
                     $("#buttonsRow").find("button[color-switcher-class='" + tagName + "']").text(buttonName).addClass(status);
                 }
             }
+
+            //If the current tag is visible
             var visible = _m.tag[tagName]["visible"];
             if (!visible) {
+                //If the button is not visible, hide it
                 $("#buttonsRow").find("button[color-switcher-class='" + tagName + "']").hide();
             }
         }
     } else {
+        //If there are no buttons yet, try again in 3 seconds
         setTimeout(updateSwitchers, 3000);
     }
 };
 
 
-
-
+/**
+ * Updates the .singleTagContent in the manualPopover. It handles their visibility
+ * based on the 'visible' property and sets the button text according to either
+ * the tagName or a predefined buttonName that can override the tagName.
+ *
+ * @method updateManualPopover
+ */
 var updateManualPopover = function() {
+    //For each of the tagNames defined in the markus.tag module
     for (var tagName in _m.tag) {
+        //get the predefined buttonName or just use the tagName
         var buttonName = _m.tag[tagName]["buttonName"] || tagName;
+
+        //If it cant find a button witht the type of tagName in the manualPopover
         if ($("#manualPopover .singleTagContent").find("button[_type='" + tagName + "']").length == 0) {
+            //We add one if it can't be found
             $("#manualPopover .singleTagContent").prepend('<button type="button" _type="' + tagName + '" class="btn btn-xs switch ' + tagName + ' switcher noColor">' + buttonName + '</button>');
         } else {
             if (_m.tag[tagName]["buttonName"]) {
+                //If it can be found we set the buttonName if it has been explicitly set (predefined)
                 $("#manualPopover .singleTagContent").find("button[_type='" + tagName + "']").text(buttonName);
             }
         }
+
+        //Hide or show the button based on their 'visible' status
         var visible = _m.tag[tagName]["visible"];
         if (!visible) {
             $("#manualPopover .singleTagContent").find("button[_type='" + tagName + "']").hide();
         }
     }
 };
-
-
-
-
 
 /*
 var attr = $(this).attr('name');
@@ -59,10 +110,23 @@ if (typeof attr !== typeof undefined && attr !== false) {
 }
 */
 
+/**
+ * This method generates all the necessary CSS classes for a new tagType. It will Also
+ * overwrite already existing definitions so you can use this to redefine the styling of existing
+ * tags too.
+ *
+ * @method newTagCSS
+ * @param  {String} tagName    the name of the tagClass we want to generate in CSS
+ * @param  {String} buttonName the name we want the button to have for this tagCSS
+ * @param  {String} tagColor   16 bit hex color (e.g. ``#FF0000` = red.) you can use css shortcuts like `red` or `#f00`
+ */
 var newTagCSS = function(tagName, buttonName, tagColor) {
+    //before adding a new tagCSS definition, remove any already existing definition for this tagName
     removeTagCSS(tagName);
     buttonName = buttonName || tagName;
     _m.tag = _m.tag || {};
+
+    //If the tag has not been defined yet, we define an object for it in the markus.tag global object
     if (_m.tag[tagName] == null) {
         _m.tag[tagName] = {
             color: tagColor,
@@ -71,16 +135,25 @@ var newTagCSS = function(tagName, buttonName, tagColor) {
             status: ""
         };
     } else {
+        //If it has already been defined, overwrite the color setting to the color we apparently want.
         _m.tag[tagName].color = tagColor;
     }
+
+    //Set the markus.tagCSS object to an empty array that we're going to redefine
     _m.tagCSS[tagName] = [];
+
+    //we set both the border and background for this button to the provided tagColor
     $.stylesheet('button.' + tagName, ['background-color', 'border-color'], tagColor);
+    //set the foreground color (i.e. text) to white
     $.stylesheet('button.' + tagName, ['color'], "#fff");
+
+    //Also push the foreground color rule into the markus.tagCSS array
     _m.tagCSS[tagName].push({
         tagName: 'button.' + tagName,
         cssKey: ["color"],
         cssValue: "#fff"
     });
+    //Now push the background and border color rule into the markus.tagCSS array
     _m.tagCSS[tagName].push({
         tagName: 'button.' + tagName,
         cssKey: ['background-color', 'border-color'],
@@ -88,9 +161,12 @@ var newTagCSS = function(tagName, buttonName, tagColor) {
     });
 
     // tagCSS['.'+tagName].push($.stylesheet('.'+tagName).rules()[0].cssText);
+
+    //We set the color for any element that just has the class of `tagName` to the tagColor
     $.stylesheet('.' + tagName, {
         "color": tagColor
     });
+    //Duplicate the above rule into the markus.tagCSS array
     _m.tagCSS[tagName].push({
         tagName: '.' + tagName,
         cssKey: ["color"],
@@ -98,20 +174,18 @@ var newTagCSS = function(tagName, buttonName, tagColor) {
     });
 
 
-    // $ss.css(,tagColor);
-    // console.log('.selected.'+tagName+"'");
+    //set the background color and border color of a selected and tagName element to tagColor
     $.stylesheet('.selected.' + tagName, ['background-color', 'border-color'], tagColor);
-
-
+    //Duplicate the above rule into the markus.tagCSS array
     _m.tagCSS[tagName].push({
         tagName: '.selected.' + tagName,
         cssKey: ['background-color', 'border-color'],
         cssValue: tagColor
     });
 
+    //Set the border color of the 'bordered' and tagname element to the provided tagColor
     $.stylesheet('.bordered.' + tagName, ['border-color'], tagColor);
-
-
+    //Duplicate the above rule into the markus.tagCSS array
     _m.tagCSS[tagName].push({
         tagName: '.bordered.' + tagName,
         cssKey: ['border-color'],
@@ -120,12 +194,15 @@ var newTagCSS = function(tagName, buttonName, tagColor) {
 
 };
 
-
-
-
-
-
+/**
+ * Called before defining new CSS rules for a tagType. This removes any pre-existing rules for
+ * the provided tagType from the markus.tag and markus.tagCSS objects.
+ *
+ * @method removeTagCSS
+ * @param  {String} tagName The tag you want to remove
+ */
 var removeTagCSS = function(tagName) {
+    //Remove the CSS for the reference from both the markus.tagCSS and markus.tag object
     delete _m.tagCSS[tagName]
     ;
     delete _m.tag[tagName]
@@ -133,27 +210,29 @@ var removeTagCSS = function(tagName) {
 };
 
 
-
-
-
 var loadCSSFromCSS_TagAtt = function() {
+    //find the tagCSS and tag attributes of the doc
     var tagCSSAttr = $(".doc").attr("tagCSS");
     var tagAttr = $(".doc").attr("tag");
 
+    //If the found variables are indeed actually populated with data, we try to populate them
     if ((typeof tagCSSAttr !== typeof undefined && tagCSSAttr !== false) && (typeof tagAttr === typeof undefined || tagAttr === false || tagAttr === "{}")) {
+        //Read the tagCSS by evaluating JSON from the tagCSS attr.
         var tagCSS = _m.tagCSS = $.evalJSON($(".doc").attr("tagCSS"));
         var tagRebuilt = {};
 
+        //For each of the predefined tags we re-add them back to the stylesheet
         for (var tag in _m.tagCSS) {
             var cssArray = tagCSS[tag];
             for (var cssIndex in cssArray) {
                 css = cssArray[cssIndex];
+                //We re-create the css rules
                 if (css.tagName == "." + tag) {
                     newTagCSS(css.tagName, css.tagName, css.cssValue);
-
                 }
             }
         }
+        //redefine the document attribute tag to the JSON stringified version of the markus.tag object
         $(".doc").attr("tag", JSON.stringify(_m.tag));
         tagAttr = $(".doc").attr("tag");
     }
@@ -166,7 +245,7 @@ var loadCSSFromCSS_TagAtt = function() {
             for (var cssIndex in cssArray) {
                 css = cssArray[cssIndex];
                 $.stylesheet(css.tagName, css.cssKey, css.cssValue);
-            //style += $.stylesheet(css.tagName).rules()[0].cssText;  
+            //style += $.stylesheet(css.tagName).rules()[0].cssText;
             }
         }
     }
@@ -256,7 +335,7 @@ var autoDownload = function(fileEntry, suffix) {
 };
 
 var saveSave = function(filename, _fn) {
-    
+
     $("body").prepend($("<span id='saving_temp' style='display:none'></span>"));
 
     var div = $("#saving_temp");
@@ -269,7 +348,7 @@ var saveSave = function(filename, _fn) {
     var saveContent = $("#saving_temp").html();
     saveFile(filename, _m.util.recoverAllExistTag(saveContent), _fn
 
-    
+
     );
     $("#saving_temp").remove();
 
