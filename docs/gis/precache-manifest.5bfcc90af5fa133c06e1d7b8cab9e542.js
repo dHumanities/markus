d029771163821237d8b9250bb45141eafe61fb52
DHumanities <5020845+dHumanities@users.noreply.github.com>
2020-08-28 21:46:29 +0200

    Add files via upload

---
+++ docs/gis/precache-manifest.5bfcc90af5fa133c06e1d7b8cab9e542.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ecc3619174b7a4ff0c6",
+    "url": "/css/app.c64f7c08.css"
+  },
+  {
+    "revision": "5ba1ca4df13fb95f598c",
+    "url": "/css/chunk-vendors.d5eae6b3.css"
+  },
+  {
+    "revision": "ac746adb8017a411bc1c01430b44252d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ecc3619174b7a4ff0c6",
+    "url": "/js/app.fe293d67.js"
+  },
+  {
+    "revision": "5ba1ca4df13fb95f598c",
+    "url": "/js/chunk-vendors.d616bf06.js"
+  },
+  {
+    "revision": "577e328f166b8e6d08b4c120d900ce44",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
